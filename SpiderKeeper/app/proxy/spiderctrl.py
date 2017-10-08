660c838f13b16f8e9f99d8e89c872038ee3f319a
Sergey <biatov.s@gmail.com>
2017-10-08 23:47:49 +0300

    Apply arguments
    Apply arguments into post request

--- SpiderKeeper/app/proxy/spiderctrl.py
+++ SpiderKeeper/app/proxy/spiderctrl.py
@@ -139,7 +139,7 @@ class SpiderAgent():
         spider_name = job_instance.spider_name
         arguments = {}
         if job_instance.spider_arguments:
-            arguments = dict(map(lambda x: x.split("="), job_instance.spider_arguments.split(",")))
+            arguments = self.set_arguments(job_instance.spider_arguments)
         threshold = 0
         daemon_size = len(self.spider_service_instances)
         if job_instance.priority == JobPriority.HIGH:
