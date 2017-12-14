cdedae36f2f235c7ab9e49ddf0b0ba8a8ff976ad
DormyMo <modongming91@gmail.com>
2017-12-14 15:16:41 +0800

    optimized setup file optimized reload spider job

--- SpiderKeeper/app/schedulers/common.py
+++ SpiderKeeper/app/schedulers/common.py
@@ -47,26 +47,30 @@ def reload_runnable_spider_job_execution():
     :return:
     '''
     running_job_ids = set([job.id for job in scheduler.get_jobs()])
-    app.logger.debug('[running_job_ids] %s' % ','.join(running_job_ids))
+    # app.logger.debug('[running_job_ids] %s' % ','.join(running_job_ids))
     available_job_ids = set()
     # add new job to schedule
     for job_instance in JobInstance.query.filter_by(enabled=0, run_type="periodic").all():
         job_id = "spider_job_%s:%s" % (job_instance.id, int(time.mktime(job_instance.date_modified.timetuple())))
         available_job_ids.add(job_id)
         if job_id not in running_job_ids:
-            scheduler.add_job(run_spider_job,
-                              args=(job_instance.id,),
-                              trigger='cron',
-                              id=job_id,
-                              minute=job_instance.cron_minutes,
-                              hour=job_instance.cron_hour,
-                              day=job_instance.cron_day_of_month,
-                              day_of_week=job_instance.cron_day_of_week,
-                              month=job_instance.cron_month,
-                              second=0,
-                              max_instances=999,
-                              misfire_grace_time=60 * 60,
-                              coalesce=True)
+            try:
+                scheduler.add_job(run_spider_job,
+                                  args=(job_instance.id,),
+                                  trigger='cron',
+                                  id=job_id,
+                                  minute=job_instance.cron_minutes,
+                                  hour=job_instance.cron_hour,
+                                  day=job_instance.cron_day_of_month,
+                                  day_of_week=job_instance.cron_day_of_week,
+                                  month=job_instance.cron_month,
+                                  second=0,
+                                  max_instances=999,
+                                  misfire_grace_time=60 * 60,
+                                  coalesce=True)
+            except Exception as e:
+                app.logger.error(
+                    '[load_spider_job] failed {} {},may be cron expression format error '.format(job_id, str(e)))
             app.logger.info('[load_spider_job][project:%s][spider_name:%s][job_instance_id:%s][job_id:%s]' % (
                 job_instance.project_id, job_instance.spider_name, job_instance.id, job_id))
     # remove invalid jobs
