fa358658163aff0604dac44091e89009dcda10b0
Ioann Materov <ivan.materov@saritasa.com>
2023-03-13 15:19:10 +0700

    Update code after updating deps versions

--- SpiderKeeper/app/__init__.py
+++ SpiderKeeper/app/__init__.py
@@ -81,8 +81,8 @@ from SpiderKeeper.app.spider.model import *
 
 
 def init_database():
-    db.init_app(app)
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
 
 # regist spider service proxy
